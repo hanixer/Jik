b60e3b018476f73248dc747a1555cac1e84706fc
Hanixer <jijer@jiji.com>
2020-01-31 22:16:03 +0200

    Changed hexDump, formatted code.

--- c/memory.c
+++ c/memory.c
@@ -2,42 +2,42 @@
 #include <windows.h>
 #include <stdio.h>
 
+extern ptr_t **globRootsTable;
 
-extern ptr_t** globRootsTable;
-
-uint64_t* freePointer;
+uint64_t *freePointer;
 
 /// Points to the beginning of FromSpace.
-uint64_t* fromSpaceBegin;
+uint64_t *fromSpaceBegin;
 
 /// Points to one memory location past the FromSpace.
-uint64_t* fromSpaceEnd;
+uint64_t *fromSpaceEnd;
 
 /// Points to the beginning of ToSpace.
-static uint64_t* toSpaceBegin;
+static uint64_t *toSpaceBegin;
 
 /// Points to one memory location past the ToSpace.
-static uint64_t* toSpaceEnd;
+static uint64_t *toSpaceEnd;
 
 /// Pointers used in copying of live data.
-static uint64_t* copyPtrBegin;
-static uint64_t* copyPtrEnd;
+static uint64_t *copyPtrBegin;
+static uint64_t *copyPtrEnd;
 
+uint64_t *rootStackBegin;
 
-uint64_t* rootStackBegin;
-
-char* allocateProtectedSpace(int size) {
+char *allocateProtectedSpace(int size)
+{
 	SYSTEM_INFO si;
 	GetSystemInfo(&si);
 	int pageSize = si.dwPageSize;
 	int aligned = (size + pageSize - 1) / pageSize * pageSize;
 	int total = aligned + pageSize * 2;
-	char* pBase = (char*) VirtualAlloc(0, total, MEM_RESERVE, PAGE_NOACCESS);
-	char* pSpace = (char*) VirtualAlloc(pBase + pageSize, aligned, MEM_COMMIT, PAGE_READWRITE);
+	char *pBase = (char *)VirtualAlloc(0, total, MEM_RESERVE, PAGE_NOACCESS);
+	char *pSpace = (char *)VirtualAlloc(pBase + pageSize, aligned, MEM_COMMIT, PAGE_READWRITE);
 	return pSpace;
 }
 
-void deallocateProtectedSpace(char* ptr, int size) {
+void deallocateProtectedSpace(char *ptr, int size)
+{
 	SYSTEM_INFO si;
 	GetSystemInfo(&si);
 	int pageSize = si.dwPageSize;
@@ -46,33 +46,38 @@ void deallocateProtectedSpace(char* ptr, int size) {
 	VirtualFree(ptr - aligned, total, MEM_RELEASE);
 }
 
-void gcInitialize(uint64_t heapSize, uint64_t rootStackSize) {
-    char* pointer = allocateProtectedSpace(heapSize);
-    freePointer = (uint64_t*) pointer;
-    fromSpaceBegin = freePointer;
-    fromSpaceEnd = (uint64_t*) (pointer + heapSize);
+void gcInitialize(uint64_t heapSize, uint64_t rootStackSize)
+{
+	char *pointer = allocateProtectedSpace(heapSize);
+	freePointer = (uint64_t *)pointer;
+	fromSpaceBegin = freePointer;
+	fromSpaceEnd = (uint64_t *)(pointer + heapSize);
 
 	pointer = allocateProtectedSpace(heapSize);
-    toSpaceBegin = (uint64_t*) pointer;
-	toSpaceEnd = (uint64_t*) (pointer + heapSize);
+	toSpaceBegin = (uint64_t *)pointer;
+	toSpaceEnd = (uint64_t *)(pointer + heapSize);
 
 	pointer = allocateProtectedSpace(rootStackSize);
-	rootStackBegin = (uint64_t*) pointer;
+	rootStackBegin = (uint64_t *)pointer;
 	rootStackBegin[0] = 0;
 }
 
 /// Cheney copying algorithm.
-void copyData(ptr_t* p) {
-	if (isVector(*p)) {
+void copyData(ptr_t *p)
+{
+	if (isVector(*p))
+	{
 		ptr_t pHeap = (*p - vectorTag);
-		if (pHeap & 1) {
+		if (pHeap & 1)
+		{
 			// forward pointer.
 			*p = (pHeap - 1) + vectorTag;
 		}
-		else {
-			ptr_t* pFrom = (ptr_t*)pHeap;
+		else
+		{
+			ptr_t *pFrom = (ptr_t *)pHeap;
 			ptr_t size = ((*pFrom) >> fixnumShift) + 1;
-			ptr_t pTo = (ptr_t) copyPtrEnd;
+			ptr_t pTo = (ptr_t)copyPtrEnd;
 			memcpy(copyPtrEnd, pFrom, size * wordSize);
 			*pFrom = pTo | 1; // Add forward bit.
 			copyPtrEnd += size;
@@ -85,15 +90,16 @@ void copyData(ptr_t* p) {
 	// }
 }
 
-void hexDump(const char * desc, const void * addr, const int len);
+void hexDump(const char *desc, const void *addr, const int len);
 
-void collect(uint64_t* rootStack, int64_t bytesNeeded) {
-    printf("--- we are in collect\n");
-    printf("--- freePointer    = %p\n", freePointer);
-    printf("--- fromSpaceBegin = %p\n", fromSpaceBegin);
-    printf("--- fromSpaceEnd   = %p\n", fromSpaceEnd);
-    printf("--- stack          = %p\n", rootStack);
-    printf("--- size           = %d\n", bytesNeeded);
+void collect(uint64_t *rootStack, int64_t bytesNeeded)
+{
+	printf("--- we are in collect\n");
+	printf("--- freePointer    = %p\n", freePointer);
+	printf("--- fromSpaceBegin = %p\n", fromSpaceBegin);
+	printf("--- fromSpaceEnd   = %p\n", fromSpaceEnd);
+	printf("--- stack          = %p\n", rootStack);
+	printf("--- size           = %d\n", bytesNeeded);
 	hexDump("from space", fromSpaceBegin, (fromSpaceEnd - fromSpaceBegin) * wordSize);
 	hexDump("to space", toSpaceBegin, (toSpaceEnd - toSpaceBegin) * wordSize);
 
@@ -101,22 +107,25 @@ void collect(uint64_t* rootStack, int64_t bytesNeeded) {
 	copyPtrEnd = toSpaceBegin;
 
 	// Copy root stack data.
-	for (uint64_t* curr = rootStack; *curr != 0; curr--) {
+	for (uint64_t *curr = rootStack; *curr != 0; curr--)
+	{
 		copyData(curr);
 	}
 
 	// Copy data pointed by globals.
-	for (int i = 0; globRootsTable[i] != 0; i++) {
+	for (int i = 0; globRootsTable[i] != 0; i++)
+	{
 		copyData(globRootsTable[i]);
 	}
 
-	while (copyPtrBegin < copyPtrEnd) {
+	while (copyPtrBegin < copyPtrEnd)
+	{
 		copyData(copyPtrBegin);
 		copyPtrBegin++;
 	}
 
-	uint64_t* pBegin = fromSpaceBegin;
-	uint64_t* pEnd = fromSpaceEnd;
+	uint64_t *pBegin = fromSpaceBegin;
+	uint64_t *pEnd = fromSpaceEnd;
 
 	fromSpaceBegin = toSpaceBegin;
 	fromSpaceEnd = toSpaceEnd;
@@ -129,60 +138,43 @@ void collect(uint64_t* rootStack, int64_t bytesNeeded) {
 	hexDump("to space", toSpaceBegin, (toSpaceEnd - toSpaceBegin) * wordSize);
 }
 
-void hexDump (const char * desc, const void * addr, const int len) {
-// Copyright 2018 Dominik Liebler
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-    int i;
-    unsigned char buff[17];
-    const unsigned char * pc = (const unsigned char *)addr;
+void hexDump(const char *desc, const void *addr, const int len)
+{
+	if (desc)
+	{
+		printf("%s:\n", desc);
+	}
 
-    // Output description if given.
+	const unsigned char *pch = (const unsigned char *)addr;
 
-    if (desc != NULL)
-        printf ("%s:\n", desc);
+	// Process by 16 bytes.
+	for (size_t i = 0; i < len; i += 16)
+	{
+		// Print address.
+		printf("0x%p   ", pch + i);
 
-    // Length checks.
+		// Print each byte.
+		for (size_t j = 0; j < 16; j++)
+		{
+			if (j == 8) printf(" ");
 
-    if (len == 0) {
-        printf("  ZERO LENGTH\n");
-        return;
-    }
-    else if (len < 0) {
-        printf("  NEGATIVE LENGTH: %d\n", len);
-        return;
-    }
+			printf("%02x ", pch[i + j]);
+		}
 
-    // Process every byte in the data.
-    for (i = 0; i < len; i++) {
-        // Multiple of 16 means new line (with line offset).
-        if ((i % 16) == 0) {
-            // Don't print ASCII buffer for the "zeroth" line.
-            if (i != 0)
-                printf ("  %s\n", buff);
+		printf("  ");
 
-            // Output the offset.
-            printf ("%08p ", pc + i);
-        }
+		// Print quads.
+		for (size_t j = 0; j < 2; j++)
+		{
+			const uint64_t* pu64 = (const uint64_t*)(pch + i);
+			const uint64_t* pu642 = pu64 + 1;
+			uint64_t va = pu64[j];
+			printf("0x%016x ", pu64[j]);
+		}
 
-        // Now the hex code for the specific character.
-        printf (" %02x", pc[i]);
 
-        // And buffer a printable ASCII character for later.
-        if ((pc[i] < 0x20) || (pc[i] > 0x7e)) // isprint() may be better.
-            buff[i % 16] = '.';
-        else
-            buff[i % 16] = pc[i];
-        buff[(i % 16) + 1] = '\0';
-    }
-
-    // Pad out last line if not exactly 16 characters.
-    while ((i % 16) != 0) {
-        printf ("   ");
-        i++;
-    }
-
-    // And print the final ASCII buffer.
-    printf ("  %s\n", buff);
+		// New line.
+		printf("\n");
+	}
 }
+
--- c/runtime.c
+++ c/runtime.c
@@ -10,51 +10,76 @@
 #include "objects.h"
 
 extern int schemeEntry();
-extern char* globVarNameTable[];
+extern char *globVarNameTable[];
 
 static void printPair(ptr_t p);
 static void printVector(ptr_t p);
 static void printString(ptr_t p);
-char* copyString(ptr_t p);
+char *copyString(ptr_t p);
 
-void printPtr(ptr_t p) {
-    if (isFixnum(p)) {
-        int n = (int) p;
+void printPtr(ptr_t p)
+{
+    if (isFixnum(p))
+    {
+        int n = (int)p;
         printf("%d", n >> fixnumShift);
-    } else if (p == falseLiteral) {
+    }
+    else if (p == falseLiteral)
+    {
         printf("#f");
-    } else if (p == trueLiteral) {
+    }
+    else if (p == trueLiteral)
+    {
         printf("#t");
-    } else if (p == nilLiteral) {
+    }
+    else if (p == nilLiteral)
+    {
         printf("()");
-    } else if ((p & charMask) == charTag) {
+    }
+    else if ((p & charMask) == charTag)
+    {
         char stringg[2] = {0, 0};
         stringg[0] = p >> charShift;
         printf("#\\%s", stringg);
-    } else if (isPair(p)) {
+    }
+    else if (isPair(p))
+    {
         printPair(p);
-    } else if (isVector(p)) {
+    }
+    else if (isVector(p))
+    {
         printVector(p);
-    } else if (isString(p)) {
+    }
+    else if (isString(p))
+    {
         printString(p);
-    } else if (isEof(p)) {
+    }
+    else if (isEof(p))
+    {
         printf("#!eof");
-    } else {
+    }
+    else
+    {
         printf("<unknown 0x%08x>", p);
     }
     fflush(stdout);
 }
 
-static void printPair(ptr_t p) {
+static void printPair(ptr_t p)
+{
     printf("(");
     printPtr(car(p));
     ptr_t curr = cdr(p);
-    while (!isNil(curr)) {
-        if (isPair(curr)) {
+    while (!isNil(curr))
+    {
+        if (isPair(curr))
+        {
             printf(" ");
             printPtr(car(curr));
             curr = cdr(curr);
-        } else {
+        }
+        else
+        {
             printf(" . ");
             printPtr(curr);
             break;
@@ -63,42 +88,50 @@ static void printPair(ptr_t p) {
     printf(")");
 }
 
-static void printVector(ptr_t p) {
+static void printVector(ptr_t p)
+{
     printf("#(");
-    for (int i = 0; i < vectorSize(p); ++i) {
+    for (int i = 0; i < vectorSize(p); ++i)
+    {
         printPtr(vectorRef(p, i));
-        if (i < vectorSize(p) - 1) {
+        if (i < vectorSize(p) - 1)
+        {
             printf(" ");
         }
     }
     printf(")");
 }
 
-static void printString(ptr_t p) {
+static void printString(ptr_t p)
+{
     printf("\"");
-    for (int i = 0; i < stringSize(p); ++i) {
+    for (int i = 0; i < stringSize(p); ++i)
+    {
         printf("%c", stringRef(p, i));
     }
     printf("\"");
 }
 
-ptr_t s_write(ptr_t fd, ptr_t str, ptr_t len) {
+ptr_t s_write(ptr_t fd, ptr_t str, ptr_t len)
+{
     int cfd = fixnumToInt(fd);
-    char* cstr = stringData(str);
+    char *cstr = stringData(str);
     int clen = fixnumToInt(len);
     int bytes = write(cfd, cstr, clen);
     return intToFixnum(bytes);
 }
 
-ptr_t s_read(ptr_t fd, ptr_t str, ptr_t len) {
+ptr_t s_read(ptr_t fd, ptr_t str, ptr_t len)
+{
     int cfd = fixnumToInt(fd);
-    char* cstr = stringData(str);
+    char *cstr = stringData(str);
     int clen = fixnumToInt(len);
     int bytes = read(cfd, cstr, clen);
     return intToFixnum(bytes);
 }
 
-ptr_t s_print6args(ptr_t a1, ptr_t a2, ptr_t a3, ptr_t a4, ptr_t a5, ptr_t a6) {
+ptr_t s_print6args(ptr_t a1, ptr_t a2, ptr_t a3, ptr_t a4, ptr_t a5, ptr_t a6)
+{
     printPtr(a1);
     printPtr(a2);
     printPtr(a3);
@@ -109,18 +142,22 @@ ptr_t s_print6args(ptr_t a1, ptr_t a2, ptr_t a3, ptr_t a4, ptr_t a5, ptr_t a6) {
     return 0;
 }
 
-void asmError() {
+void asmError()
+{
     fprintf(stderr, "error, probably bad procedure call");
     exit(1);
 }
 
-void globVarError(char* varAddress) {
+void globVarError(char *varAddress)
+{
     // see printGlobalOriginals in CodePrinter.fs
-    for (int i = 0; globVarNameTable[i] != 0; i += 2) {
+    for (int i = 0; globVarNameTable[i] != 0; i += 2)
+    {
         // the first entry is an address
-        if (globVarNameTable[i] == varAddress) {
+        if (globVarNameTable[i] == varAddress)
+        {
             // the second entry is a string
-            char* str = globVarNameTable[i + 1];
+            char *str = globVarNameTable[i + 1];
             fprintf(stderr, "global variable '%s' is not initialized\n", str);
             exit(1);
         }
@@ -130,43 +167,49 @@ void globVarError(char* varAddress) {
     exit(1);
 }
 
-void s_error(ptr_t x) {
+void s_error(ptr_t x)
+{
     fprintf(stderr, "error!\n");
     printf("error!\n");
     printPtr(x);
     exit(1);
 }
 
-char* copyString(ptr_t p) {
+char *copyString(ptr_t p)
+{
     int size = stringSize(p);
-    char* str = stringData(p);
-    char* newStr = malloc(size + 1);
-    for (int i = 0; i < size; ++i) {
+    char *str = stringData(p);
+    char *newStr = malloc(size + 1);
+    for (int i = 0; i < size; ++i)
+    {
         newStr[i] = str[i];
     }
     newStr[size] = 0;
     return newStr;
 }
 
-ptr_t s_openFileW(ptr_t filename) {
-    char* str = copyString(filename);
+ptr_t s_openFileW(ptr_t filename)
+{
+    char *str = copyString(filename);
     int fd = open(str, O_CREAT | O_WRONLY);
     int err = errno;
-    char* estr = strerror(err);
+    char *estr = strerror(err);
     free(str);
     return intToFixnum(fd);
 }
 
-ptr_t s_openFileR(ptr_t filename) {
-    char* str = copyString(filename);
+ptr_t s_openFileR(ptr_t filename)
+{
+    char *str = copyString(filename);
     int fd = open(str, O_RDONLY);
     int err = errno;
-    char* estr = strerror(err);
+    char *estr = strerror(err);
     free(str);
     return intToFixnum(fd);
 }
 
-ptr_t s_closeFile(ptr_t fd) {
+ptr_t s_closeFile(ptr_t fd)
+{
     int cfd = fixnumToInt(fd);
     int ret = close(fd);
     return intToFixnum(ret);
@@ -174,7 +217,8 @@ ptr_t s_closeFile(ptr_t fd) {
 
 static int allocations[5];
 
-void printAllocInfo() {
+void printAllocInfo()
+{
     printf("Allocations info:\n");
     printf("vector: %d\n", allocations[0]);
     printf("string: %d\n", allocations[1]);
@@ -183,19 +227,22 @@ void printAllocInfo() {
     printf("other: %d\n", allocations[5]);
 }
 
-void s_exit(ptr_t p) {
+void s_exit(ptr_t p)
+{
     // printAllocInfo();
     ExitProcess((UINT)fixnumToInt(p));
 }
 
-void checkHeapSpaceAvailable(int which) {
+void checkHeapSpaceAvailable(int which)
+{
     allocations[which]++;
 }
 
-int main() {
+int main()
+{
     int stackSize = 10 * 16 * 4096;
-    char* stack = allocateProtectedSpace(stackSize);
-    char* stackHigherAddr = stack + stackSize - 2 * wordSize;
+    char *stack = allocateProtectedSpace(stackSize);
+    char *stackHigherAddr = stack + stackSize - 2 * wordSize;
     int heapSize = 40;
     // int heapSize = 100 * 1024 * 4096;
     int rootStackSize = stackSize;
